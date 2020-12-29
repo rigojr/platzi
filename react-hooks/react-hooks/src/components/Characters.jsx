e3900792e7313074e7260eca19f9bc7fb4d24821
Rigo <rigojunnior@gmail.com>
2020-12-29 06:03:41 -0400

    react hooks useReducer

--- react-hooks/react-hooks/src/components/Characters.jsx
+++ react-hooks/react-hooks/src/components/Characters.jsx
@@ -1,8 +1,25 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useReducer } from 'react';
+
+const initialState = {
+    favorites: []
+}
+
+const favoriteReducer = (state, action) => {
+    switch (action.type) {
+        case 'ADD_TO_FAVORITE':
+            return {
+                ...state,
+                favorites: [...state.favorites, action.payload]
+            }
+        default:
+            return state
+    }
+}
 
 const Characters = () => {
 
     const [characters, setCharacters] = useState([])
+    const [favorites, dispatch] = useReducer(favoriteReducer, initialState)
 
     useEffect( () => {
         fetch('https://rickandmortyapi.com/api/character/')
@@ -10,16 +27,30 @@ const Characters = () => {
         .then( data => setCharacters(data.results))
     }, [] ) // Solo va a ser render la primera vez
 
-    console.log(characters)
+    const handleClick = favorite => {
+        dispatch({
+            type: 'ADD_TO_FAVORITE',
+            payload: favorite
+        })
+    }
 
     return (
         <div className="Characters">
+            <h2>Personajes Favoritos</h2>
+            {
+                favorites.favorites.map( favorite => <h2 key={favorite.id}>{favorite.name}</h2>)
+            }
+            <h2>Todos los personajes</h2>
             {
                 characters.map( character => (
-                    <h2>{character.name}</h2>
+                    <React.Fragment key={character.name}>
+                        <h2>{character.name}</h2>
+                        <button 
+                            type="button"
+                            onClick={() => handleClick(character)}>Agregar a Favorito</button>
+                    </React.Fragment>
                 ))
             }
-            <h2>{}</h2>
         </div>
     );
 }
