4702e3be1a247bbc5e89b663daa33821cc24e808
Rigo <rigojunnior@gmail.com>
2020-12-29 06:37:03 -0400

    react hooks useMemo

--- react-hooks/react-hooks/src/components/Characters.jsx
+++ react-hooks/react-hooks/src/components/Characters.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useReducer } from 'react';
+import React, { useState, useEffect, useReducer, useMemo } from 'react';
 
 const initialState = {
     favorites: []
@@ -20,6 +20,7 @@ const Characters = () => {
 
     const [characters, setCharacters] = useState([])
     const [favorites, dispatch] = useReducer(favoriteReducer, initialState)
+    const [search, setSearch] = useState('')
 
     useEffect( () => {
         fetch('https://rickandmortyapi.com/api/character/')
@@ -34,15 +35,30 @@ const Characters = () => {
         })
     }
 
+    const handleSearch = event => {
+        setSearch(event.target.value)
+    }
+
+    // const filteredUsers = characters.filter( (user) => 
+    //     user.name.toLowerCase().includes(search.toLocaleLowerCase()))
+
+    const filteredUsers = useMemo( () => 
+        characters.filter( (user) => 
+        user.name.toLowerCase().includes(search.toLocaleLowerCase()))
+    , [characters, search]) // elementos donde se va a escuchar 
+
     return (
         <div className="Characters">
+            <div className="busqueda">
+                <input type="text" value={search} onChange={handleSearch}/>
+            </div>
             <h2>Personajes Favoritos</h2>
             {
                 favorites.favorites.map( favorite => <h2 key={favorite.id}>{favorite.name}</h2>)
             }
             <h2>Todos los personajes</h2>
             {
-                characters.map( character => (
+                filteredUsers.map( character => (
                     <React.Fragment key={character.name}>
                         <h2>{character.name}</h2>
                         <button 
