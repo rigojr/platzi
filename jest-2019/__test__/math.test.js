68230ca91913bb98f3e85f77d9bb29af85ac9129
Rigo <rigojunnior@gmail.com>
2021-01-06 07:25:40 -0400

    testing normal math operations

--- jest-2019/__test__/math.test.js
+++ jest-2019/__test__/math.test.js
@@ -4,4 +4,17 @@ describe('Math.js', () => {
     it('should add', () => {
         expect( operation.sumar(1,1)).toBe(2);
     });
+    
+    it('should *', () => {
+        expect( operation.multiplicar(2,2)).toBe(4);
+    });
+
+    it('should /', () => {
+        expect( operation.dividir(2,2) ).toBe(1);
+    });
+
+    it('should -', () => {
+        expect( operation.restar(2,2) ).toBe(0);
+    });
+
 });
