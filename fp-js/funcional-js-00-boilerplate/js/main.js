c7c015813834068969f7729c041a90e51568436b
Rigo <rigojunnior@gmail.com>
2021-02-22 07:43:26 -0400

    validating inputs with jQurey

--- fp-js/funcional-js-00-boilerplate/js/main.js
+++ fp-js/funcional-js-00-boilerplate/js/main.js
@@ -5,4 +5,35 @@ const compose = (...functions) => data =>
   let calories = $('#calories')
   let carbs = $('#carbs')
   let protein = $('#protein')
-  
+
+  description.keypress( () => {
+    description.removeClass('is-invalid')
+  })
+
+  calories.keypress( () => {
+    calories.removeClass('is-invalid')
+  })
+
+  carbs.keypress( () => {
+    carbs.removeClass('is-invalid')
+  })
+
+  protein.keypress( () => {
+    protein.removeClass('is-invalid')
+  })
+
+  const validateInputs = () => {
+
+    description.val() ? '' : description.addClass('is-invalid')
+    calories.val() ? '' : calories.addClass('is-invalid')
+    carbs.val() ? '' : carbs.addClass('is-invalid')
+    protein.val() ? '' : protein.addClass('is-invalid')
+
+    if( 
+      description.val() && 
+      calories.val() &&
+      carbs.val() &&
+      protein.val()
+    ) console.log("ok")
+
+  }
