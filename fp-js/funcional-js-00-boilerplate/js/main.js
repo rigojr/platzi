1e8325552aeba17da9b8d4d8486e803c1b448996
Rigo <rigojunnior@gmail.com>
2021-02-22 07:23:45 -0400

    adding id for jQuery

--- fp-js/funcional-js-00-boilerplate/js/main.js
+++ fp-js/funcional-js-00-boilerplate/js/main.js
@@ -1,2 +1,8 @@
 const compose = (...functions) => data =>
   functions.reduceRight((value, func) => func(value), data)
+
+  let description = $('#description')
+  let calories = $('#calories')
+  let carbs = $('#carbs')
+  let protein = $('#protein')
+  
