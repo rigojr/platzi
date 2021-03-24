6d74ee699926e731cc428c9c75075ad18cb2b7bf
Jose Salas <rigojunnior@gmail.com>
2021-03-24 16:46:51 -0400

    mockups and products list at home

--- react-hooks/conf-merch-ts/src/common/types.ts
+++ react-hooks/conf-merch-ts/src/common/types.ts
@@ -1,12 +1,33 @@
 import React from "react";
 
-// INTERFACES
+// REGULAR INTERFACES
+
+export interface Product {
+  id: string;
+  image: string;
+  title: string;
+  price: number;
+  description: string;
+}
+
+// COMPONENTS INTERFACES
 
 export interface ILayout {
   children?: React.ReactNode;
+}
+
+export interface IProducts {
+  products: Product[];
+}
+
+export interface IProduct {
+  product: Product;
+  key: string | number;
 }
 
 // CONSTANTS
 
 export const TITLE_WEB_APP = 'PlatziConf Merch';
 export const COPY_RIGHT_TEXT = 'Todos los derechos reservados';
+
+// TYPES
---
+++ react-hooks/conf-merch-ts/src/components/Product.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { IProduct } from '../common/types';
+
+const Product: React.FC<IProduct> = ({product, key}) => {
+  return (
+    <div className="Products-item" key={key}>
+      <img src={product.image} alt={product.title}/>
+      <div className="Product-item-info">
+        <h2>
+          {product.title} <span>$ {product.price}</span>
+        </h2>
+        <p>{product.description}</p>
+      </div>
+      <button type="button">Comprar</button>
+    </div>
+  );
+}
+
+export default Product;
