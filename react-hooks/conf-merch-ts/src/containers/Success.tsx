de12283129dd6e1a9bb7ce9ed44029b61de01458
Jose Salas <rigojunnior@gmail.com>
2021-03-30 08:17:31 -0400

    integrating google maps part 2

--- react-hooks/conf-merch-ts/src/containers/Success.tsx
+++ react-hooks/conf-merch-ts/src/containers/Success.tsx
@@ -3,17 +3,19 @@ import AppContext from '../context/AppContext';
 import '../styles/components/Success.css';
 import { IUseInitialState } from '../common/types';
 import Map from '../components/Map';
+import useGoogleAddress from '../hooks/useGoogleAddress';
 
 const Success: React.FC = () => {
   const { state } = useContext(AppContext) as IUseInitialState;
   const { buyer } = state;
+  const location = useGoogleAddress(buyer.address);
   return (
     <div className="Success">
       <div className="Success-content">
         <h2>{`${buyer.name}, Gracias por tu compra`}</h2>
         <span>Tu pedido llegara en 3 dias</span>
         <div className="Success-map">
-          <Map />
+          <Map data={location} />
         </div>
       </div>
     </div>
